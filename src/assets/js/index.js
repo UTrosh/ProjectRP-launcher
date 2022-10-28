287226f54e1227b402bcb513428e29c1b5cebc1d
UTrosh <73399986+UTrosh@users.noreply.github.com>
2022-10-28 19:38:03 +0200

    Update mod

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -22,9 +22,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n' ai pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Ce projet est le notre !", "author": "UTrosh" },
+            { "message": "Salut je suis du code.", "author": "UTrosh" },
+            { "message": "Vive le moddé !", "author": "UTrosh" }
         ]
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
